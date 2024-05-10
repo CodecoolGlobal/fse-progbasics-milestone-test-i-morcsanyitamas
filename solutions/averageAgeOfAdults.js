32658322f082b2b7bdc7a57e207468787bf156ab
morcsanyitamas <mtomi8@gmail.com>
2024-05-10 15:49:19 +0200

    expect 0 points

--- solutions/averageAgeOfAdults.js
+++ solutions/averageAgeOfAdults.js
@@ -16,7 +16,7 @@ You do not have to deal with the case, when there are only underage users in the
 */
 
 function averageAgeOfAdults(users) {
-
+  return 0;
 }
 
 module.exports = averageAgeOfAdults;
